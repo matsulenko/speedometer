cf332a8e034660bfbc7babb319ad21b5caddc24b
matsulenko <matsulenko@mail.ru>
2024-03-26 21:11:52 +0300

    Initial Commit

---
+++ Speedometer/SpeedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedometerApp.swift
+//  Speedometer
+//
+//  Created by Matsulenko on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpeedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
